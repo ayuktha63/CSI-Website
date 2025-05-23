9f196c679251c04213f248d99d6bdc1df00900ae
ayuktha63 <krishnaprasadsm63@gmail.com>
2025-05-23 12:18:55 +0530

    2 events page example

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css"; // Import AOS styles
-
+import ScrollToTop from "./components/ScrollToTop";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainHome from "./components/MainHome"; // Updated import
 import Events from "./components/Events"; // Events page
 import Team from "./components/Team";
+import Adastra from "./components/Adastra"; // Ad Astra event details
+import Software from "./components/Software";
 const App = () => {
   useEffect(() => {
     AOS.init({
@@ -19,11 +21,14 @@ const App = () => {
   return (
     <Router>
       <Header />
+      <ScrollToTop />
       <main>
         <Routes>
           <Route path="/" element={<MainHome />} /> {/* Updated Route */}
           <Route path="/events" element={<Events />} />
           <Route path="/team" element={<Team />} />
+           <Route path="/adastra" element={<Adastra />} />
+          <Route path ="/software" element={<Software />} />
         </Routes>
       </main>
       <Footer />
--- src/components/Events.js
+++ src/components/Events.js
@@ -3,6 +3,8 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "../styles/Events.css";
 import Header from "./Header";
+import { Link } from "react-router-dom";
+
 
 const Events = () => {
     const [scrollPosition, setScrollPosition] = useState(0);
@@ -42,12 +44,13 @@ const Events = () => {
                         is organizing Ad Astra, a month-long event aimed at enhancing and building a comprehensive 
                         skill set for every participant.
                     </p>
-                    <button className="read-more-btn" data-aos="fade-left">
-                        <span>Read more</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="25" viewBox="0 0 32 25" fill="none">
-                            <path d="M31.1828 13.5074C31.8313 12.8541 31.8273 11.7989 31.174 11.1504L20.5275 0.583881C19.8741 -0.0645362 18.8189 -0.0605596 18.1705 0.592763C17.522 1.24608 17.526 2.30135 18.1793 2.94977L27.6429 12.3423L18.2504 21.8058C17.602 22.4591 17.606 23.5144 18.2593 24.1628C18.9126 24.8112 19.9679 24.8073 20.6163 24.1539L31.1828 13.5074ZM0.561945 14.111L30.0062 14L29.9936 10.6667L0.549384 10.7777L0.561945 14.111Z" fill="white"/>
-                        </svg>
-                    </button>
+                    <Link to="/adastra" className="read-more-btn" data-aos="fade-left">
+                    <span>Read more</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="25" viewBox="0 0 32 25" fill="none">
+                        <path d="M31.1828 13.5074C31.8313 12.8541 31.8273 11.7989 31.174 11.1504L20.5275 0.583881C19.8741 -0.0645362 18.8189 -0.0605596 18.1705 0.592763C17.522 1.24608 17.526 2.30135 18.1793 2.94977L27.6429 12.3423L18.2504 21.8058C17.602 22.4591 17.606 23.5144 18.2593 24.1628C18.9126 24.8112 19.9679 24.8073 20.6163 24.1539L31.1828 13.5074ZM0.561945 14.111L30.0062 14L29.9936 10.6667L0.549384 10.7777L0.561945 14.111Z" fill="white"/>
+                    </svg>
+                    </Link>
+
                     <div className="event-details right-text" data-aos="fade-left">
                         <img src="/assets/calender.svg" alt="Calendar Icon" className="date-icon" />
                         01 February 2025
@@ -60,12 +63,13 @@ const Events = () => {
                     <p className="software-description" data-aos="fade-right">
                         Department of Computer Science & Engineering in association with CSI SB MBCET, Mar Baselios College of Engineering and Technology, Thiruvananthapuram is organizing an online workshop - Software Testing, Industrial Approach.
                     </p>
-                    <button className="software-btn" data-aos="fade-right">
-                        <span>Read more</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="25" viewBox="0 0 32 25" fill="none">
-                            <path d="M31.1828 13.5074C31.8313 12.8541 31.8273 11.7989 31.174 11.1504L20.5275 0.583881C19.8741 -0.0645362 18.8189 -0.0605596 18.1705 0.592763C17.522 1.24608 17.526 2.30135 18.1793 2.94977L27.6429 12.3423L18.2504 21.8058C17.602 22.4591 17.606 23.5144 18.2593 24.1628C18.9126 24.8112 19.9679 24.8073 20.6163 24.1539L31.1828 13.5074ZM0.561945 14.111L30.0062 14L29.9936 10.6667L0.549384 10.7777L0.561945 14.111Z" fill="white"/>
-                        </svg>
-                    </button>
+                    <Link to="/software" className="software-btn" data-aos="fade-left">
+                    <span>Read more</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="25" viewBox="0 0 32 25" fill="none">
+                        <path d="M31.1828 13.5074C31.8313 12.8541 31.8273 11.7989 31.174 11.1504L20.5275 0.583881C19.8741 -0.0645362 18.8189 -0.0605596 18.1705 0.592763C17.522 1.24608 17.526 2.30135 18.1793 2.94977L27.6429 12.3423L18.2504 21.8058C17.602 22.4591 17.606 23.5144 18.2593 24.1628C18.9126 24.8112 19.9679 24.8073 20.6163 24.1539L31.1828 13.5074ZM0.561945 14.111L30.0062 14L29.9936 10.6667L0.549384 10.7777L0.561945 14.111Z" fill="white"/>
+                    </svg>
+                    </Link>
+
                     <div className="event-details left-text-odd" data-aos="fade-right">
                         09 November 2024
                         <img src="/assets/calender.svg" alt="Calendar Icon" className="date-icon" />
